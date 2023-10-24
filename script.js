c20a0b321d793dd4aa5d98e0b0383c887c68dfc4
itsMeVela <velaputri681@gmail.com>
2023-10-24 23:03:10 +0700

    tombol acak warna selesai

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//https://www.youtube.com/watch?v=DLFqQ1SP8xU&list=PLFIM0718LjIWB3YRoQbQh82ZewAGtE2-3&index=8
+
 const judul = document.getElementById('judul');
 judul.style.textAlign = 'center';
 judul.style.color='black';
@@ -58,5 +60,28 @@ tAcakWarna.setAttribute('type', 'button');
 //kita tampilkan setelah tombol ubah warna
 tombol.insertAdjacentElement('afterend', tAcakWarna);
 tAcakWarna.style.marginRight='10px'; //g fungsi masih an
+
+//kita buat agar setiap kali mouse mendekat dia berubah warna
+tAcakWarna.addEventListener('mouseenter', function(){
+    tAcakWarna.style.backgroundColor='pink';   
+});
+tAcakWarna.addEventListener('mouseleave', function(){
+    tAcakWarna.style.backgroundColor='rgb(241, 241, 241)';   
+});
+
+//oke mari kita buat ketika tombol di klik dia mulai merubah warna
+tAcakWarna.addEventListener('click', function(){
+    //oke mari kita buat methode untuk mengacak nilai rgb, detai nya biasa liat link yt diatas
+    const r = Math.round(Math.random() *255 + 1);
+    // penjelasan singkatnya math.round untuk membulatkan angka
+    // math.random akan memberikan angka secara acak namun bentuknya desimal dan dengan angka acak tidak terbatas
+    // berhubung rgb dimulai dari 0-255 kita bisa kalikan math.random dengan 255 dan di tambah satu. ini hasilnya masih desimal, mangkanya kita
+    //tambahkan math.round agar menjadi satuan
+    const g = Math.round(Math.random() * 255 + 1);
+    const b = Math.round(Math.random() * 255 + 1);
+    document.body.style.backgroundColor='rgb('+ r +','+ g +','+ b +')'
+});
+// permasalahannya sekarang adalah, kenapa saat kita oprasikan tombol ubah warna acak, saat kita klik tombol ubah warna dia tidak merespon?
+
 
 
